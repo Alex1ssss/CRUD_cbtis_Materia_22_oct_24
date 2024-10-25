a74304a716e5441965ec180723f0d75c5546512a
Alex1ssss <alexissanchez99998@gmail.com>
2024-10-25 12:02:50 -0600

    Delete CRUD_cbtis_Materia_22_oct_24-v2 directory

--- CRUD_cbtis_Materia_22_oct_24-v2/DAWeb_CB_cbtis128/materia_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Materia
-# Create your views here.
-def inicio_vista(request):
-    ListadoMaterias=Materia.objects.all()
-    return render(request,"gestionarMateria.html",{"lasmaterias":ListadoMaterias})
